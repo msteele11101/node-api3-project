5022bc4d04bf00e0c164d9bf031485d7d8b09514
msteele11101 <msteele11101@gmail.com>
2020-03-19 14:45:34 -0700

    MVP

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const server = express();
+server.use(express.json());
+server.use(logger);
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+
+// Routers
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +22,14 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const method = req.method;
+	const url = req.originalUrl;
+	const timeStamp = Date.now();
+
+	console.log(`\nMethod: ${method} \nUrl: ${url} \nTime: ${timeStamp}`);
+
+	next();
+}
 
 module.exports = server;
